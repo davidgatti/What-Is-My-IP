ce30da18d73b2cf97f05a3ffec2718dd1e65f9e1
David Gatti <office@gatti.pl>
2016-07-10 18:14:20 +0200

    Let the system manage when to update the data

--- client-macos/wimi-today/TodayViewController.swift
+++ client-macos/wimi-today/TodayViewController.swift
@@ -35,12 +35,20 @@ class TodayViewController: NSViewController, NCWidgetProviding {
         return "TodayViewController"
     }
 
+	//
+	//	Let the system periodically fetch new data
+	//
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
-        // Update your data and prepare for a snapshot. Call completion handler when you are done
-        // with NoData if nothing has changed or NewData if there is new data since the last
-        // time we called you
-        completionHandler(.NoData)
-		print("ping")
+
+		//
+		//	Load the data from the server
+		//
+		getData();
+
+		//
+		//	Let the ssytem know there is new data
+		//
+        completionHandler(.NewData)
     }
 
     override func viewDidLoad() {
