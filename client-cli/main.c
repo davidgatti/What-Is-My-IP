6fa0c20f90c4b1230f6c2fe14b923d5fa2bed465
David Gatti <office@gatti.pl>
2016-07-17 15:32:50 +0200

    Rewrote all to support TLS

--- client-cli/main.c
+++ client-cli/main.c
@@ -1,143 +1,133 @@
-//
-//  main.c
-//  wimi
-//
-//  Created by David Gatti on 7/2/16.
-//  Copyright © 2016 David Gatti. All rights reserved.
-//
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/bio.h>
 
-#include <netdb.h>
-#include <netinet/in.h>
+#define APIKEY "YzUwYzQ3MzAzODdlZDJlYjUxMmZiYzk0NTc5ZjVlNTM1YzY3MjFjMjI5MDI5MDQzMzRlNDA1ODJkMmE1M2ExMQ=="
+#define HOST "wimi-test.herokuapp.com"
+#define PORT "443"
 
-#include <string.h>
-#include <time.h>
-#include <signal.h>
+int main() {
 
-int main(int argc, char **argv)
-{
-    //
-    //  Check for arguments
-    //
-    if( argc == 2 ) {
-        printf("The argument supplied is %s\n", argv[1]);
-    }
-    else if( argc > 2 ) {
-        printf("Too many arguments supplied.\n");
-    }
-    else {
-        printf("One argument expected.\n");
-    }
+	//
+	//	Initialize Variables
+	//
+	BIO* bio;
+	SSL* ssl;
+	SSL_CTX* ctx;
 
-    char host[] = "secure-earth-47559.herokuapp.com";
-    int sockfd;
-    int portno;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    
-    //
-    //  Create a socket point
-    //
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	//
+	//   Registers the available SSL/TLS ciphers and digests.
+	//
+	//   Basically start the security layer.
+	//
+	SSL_library_init();
 
-    //
-    //  -> Show error if any
-    //
-    if (sockfd < 0)
-    {
-        perror("ERROR opening socket");
-        exit(1);
-    }
+	//
+	//	Creates a new SSL_CTX object as framework to establish TLS/SSL
+	//	or DTLS enabled connections
+	//
+	ctx = SSL_CTX_new(SSLv23_client_method());
 
-    //
-    //  converts the string argument to an integer (type int).
-    //
-    portno = atoi("5000");
+	//
+	//	-> Error check
+	//
+	if (ctx == NULL)
+	{
+		printf("Ctx is null\n");
+	}
 
-    //
-    //  Provided by netdb.h
-    //
-    server = gethostbyname(host);
+	//
+	//	 Creates a new BIO chain consisting of an SSL BIO
+	//
+	bio = BIO_new_ssl_connect(ctx);
 
-    //
-    //  -> Show error if any
-    //
-    if (server == NULL)
-    {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
+	//
+	//	Retrieves the SSL pointer of BIO, it can then be manipulated
+	//	using the standard SSL library functions
+	//
+	BIO_get_ssl(bio, &ssl);
 
-    //
-    //  Fill with 0
-    //
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+	//
+	//	uses the string name to set the hostname
+	//
+	BIO_set_conn_hostname(bio, HOST ":" PORT);
 
-    //
-    //  set the type of connection to TCP/IP
-    //
-    serv_addr.sin_family = AF_INET;
+	//
+	//	 Attempts to connect the supplied BIO
+	//
+	if(BIO_do_connect(bio) <= 0)
+	{
+		printf("Failed connection\n");
+		return 1;
+	}
+	else
+	{
+		printf("Connected\n");
+	}
 
-    //
-    //  pointer to the first IP address
-    //
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+	//
+	//	Data to send to create a HTTP request.
+	//
+	char* write_buf = "POST / HTTP/1.1\r\n"
+					  "Host: " HOST "\r\n"
+					  "Authorization: Basic " APIKEY "\r\n"
+					  "Connection: close\r\n"
+					  "\r\n";
 
-    //
-    //  converts the unsigned short integer hostshort from host byte
-    //  order to network byte order.
-    //
-    serv_addr.sin_port = htons(portno);
+	//
+	//	 Attempts to write len bytes from buf to BIO
+	//
+	if(BIO_write(bio, write_buf, strlen(write_buf)) <= 0)
+	{
+		//
+		//	Handle failed write here
+		//
+		if(!BIO_should_retry(bio))
+		{
+			// Not worth implementing, but worth knowing.
+		}
 
-    //
-    //  Now connect to the server
-    //
-    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        perror("ERROR connecting");
-        exit(1);
-    }
+		//
+		//	-> Let us know about the failed write
+		//
+		printf("Failed write\n");
+	}
 
-    //
-    //  Now ask for a message from the user, this message
-    //  will be read by server
-    //
-    char ble[54] = "POST / HTTP/1.1\nAuthorization: Basic cXdld3FlOg==\n\n";
+	//
+	//	Variables used to read the response from the server
+	//
+	int size;
+	char buf[1024];
 
-    while(1)
-    {
-        //
-        //  Send message to the server
-        //
-        long w = write(sockfd, ble, strlen(ble));
-        
-        if (w < 0)
-        {
-            perror("ERROR writing to socket");
-            exit(1);
-        }
-        
-        //
-        //  Now read server response
-        //
-        char buffer[256];
-        bzero(buffer, 256);
+	//
+	//	Read the response message
+	//
+	for(;;)
+	{
+		//
+		//	Put response in a buffer of size.
+		//
+		size = BIO_read(bio, buf, 1023);
 
-        long r = read(sockfd, buffer, 255);
-        
-        if (r < 0)
-        {
-            perror("ERROR reading from socket");
-            exit(1);
-        }
-        
-        printf("%s\n\n", buffer);
+		//
+		//	If no more data, then exit the loop
+		//
+		if(size <= 0)
+		{
+		 	break;
+		}
 
-        sleep(1);
-    }
-    
-    return 0;
+		buf[size] = 0;
+
+		printf("%s", buf);
+	}
+
+	//
+	//	Clean after ourselves
+	//
+	BIO_free_all(bio);
+	SSL_CTX_free(ctx);
+
+	return 0;
 }
