1b2fb1002dfe0d95f718d33c277d3e042761983a
David Gatti <office@gatti.pl>
2016-07-24 11:34:41 +0200

    Improved the welcome page

--- server/routes/welcome.js
+++ server/routes/welcome.js
@@ -8,12 +8,10 @@ router.get('/', function(req, res, next) {
 	//
 	//	-> Render the page
 	//
-	res.render('welcome',
-		{
-			uuid: process.env.API_KEY
-		}
-	);
-
+	res.render('welcome', {
+			uuid: process.env.API_KEY,
+			url: req.protocol + '://' + req.get('host')
+	});
 });
 
 module.exports = router;
