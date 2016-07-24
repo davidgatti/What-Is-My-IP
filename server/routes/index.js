a06f48ade9c4b3a430132bd18055244ee7d0c6fc
David Gatti <davidgatti@users.noreply.github.com>
2016-07-24 15:32:36 +0200

    Fixed issue with the IP (#1)
    * Header debug
    * Testing new IP aquistion

--- server/routes/index.js
+++ server/routes/index.js
@@ -29,28 +29,7 @@ router.post('/', function(req, res, next) {
 	//	1. Create an empty variable where we are going to store
 	//	   the remote IP.
 	//
-    let ip = '';
-
-    //
-    //	2. Get the remote IP, and to make sure we get the actual user IP.
-    //
-    //	   We have to over all this hoops. Otherwise we are going to get
-    //	   the load balancer IP and any other service that sits between
-    //	   the user and our app.
-    //
-    if(req.headers['x-forwarded-for'])
-    {
-        var ipList = req.headers['x-forwarded-for'].split(",");
-        ip = ipList[ipList.length - 1];
-    }
-    else if(req.connection && req.connection.remoteAddress)
-    {
-        ip = req.connection.remoteAddress;
-    }
-    else
-    {
-        ip = req.ip;
-    }
+    let ip = req.headers['x-forwarded-for'];
 
     //
     //	3. Save the time stamp so we can tell how fresh is the IP
