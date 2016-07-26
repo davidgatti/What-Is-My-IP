253a33bdca6262e5ca98eab48da686f076b66928
David Gatti <office@gatti.pl>
2016-07-26 23:24:22 +0200

    Better organization

--- server/routes/welcome.js
+++ server/routes/welcome.js
@@ -11,7 +11,11 @@ router.get('/', function(req, res, next) {
 	res.render('welcome', {
 			uuid: process.env.API_KEY,
 			url: req.protocol + '://' + req.get('host'),
-			visited: !!parseInt(process.env.VISITED)
+			visited: !!parseInt(process.env.VISITED),
+			partials: {
+				welcome_acccess: 'welcome_access',
+				welcome_noacccess: 'welcome_noaccess'
+			}
 	});
 
 	process.env.VISITED = 1;
